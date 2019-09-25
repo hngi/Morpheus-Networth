a01e4f5af03118aa82a6e97d242db7dfe836aea6
Olufunmiladunni <olucynakin@gmail.com>
2019-09-25 08:59:58 +0100

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', ' b22_24540261_team_morpheus');
+define('DB_NAME', 'team_morpheus');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
