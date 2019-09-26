92384b451ca5f3386af4e97c1fc0c512549f31d3
Yusuf-Ismaila <54332607+Yusuf-Ismaila@users.noreply.github.com>
2019-09-26 21:38:27 +0100

    Add Asset & Liability
    Add Asset & Liability

---
+++ script.js
@@ -0,0 +1,79 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["description"] = document.getElementById("description").value;
+   
+    formData["value"] = document.getElementById("price").value;
+    
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("assetList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.description;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.value;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("description").value = "";
+    document.getElementById("price").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("description").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("price").value = selectedRow.cells[2].innerHTML;
+    
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.description;
+    selectedRow.cells[2].innerHTML = formData.value;
+   
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure you want to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("assetList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("description").value == "") {
+        isValid = false;
+        document.getElementById("descriptionValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("descriptionValidationError").classList.contains("hide"))
+            document.getElementById("descriptionValidationError").classList.add("hide");
+    }
+    return isValid;
+
+   // When the user clicks anywhere outside of the modal, close it//
+   
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+}
